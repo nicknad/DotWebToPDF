78757d51321b3ae4c47a907821e894ce93798c9a
nicknad <nadolskinick@gmail.com>
2024-09-17 20:33:04 +0200

    Changed from ExtensionMethod to Method

--- DotWebToPDFGenerator.cs
+++ DotWebToPDFGenerator.cs
@@ -1,17 +1,47 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using CommunityToolkit.Diagnostics;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.Logging;
+using Microsoft.Playwright;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DotWebToPDF;
 
 public class DotWebToPDFGenerator
 {
-    internal readonly IServiceProvider services;
-    internal readonly ILoggerFactory loggerFactory;
+    private readonly IServiceProvider _services;
+    private readonly ILoggerFactory _loggerFactory;
 
     public DotWebToPDFGenerator(IServiceProvider services, ILoggerFactory loggerFactory)
     {
-        this.services = services;
-        this.loggerFactory = loggerFactory;
+        Guard.IsNotNull(loggerFactory, nameof(loggerFactory));
+        Guard.IsNotNull(services, nameof(services));
+        this._services = services;
+        this._loggerFactory = loggerFactory;
     }
 
+    public async Task<byte[]> ToPDFAsync(string html, PagePdfOptions? pdfOptions = null)
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = true });
+        var page = await browser.NewPageAsync();
+        await page.SetContentAsync(html);
+
+        return await page.PdfAsync(pdfOptions);
+    }
+
+    public async Task<byte[]> ToPDFAsync<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] TComponent>(
+        ParameterView parameters,
+        PagePdfOptions? pdfOptions = null) where TComponent : IComponent
+    {
+        await using var htmlRenderer = new HtmlRenderer(_services, _loggerFactory);
+
+        var html = await htmlRenderer.Dispatcher.InvokeAsync(async () =>
+        {
+            var output = await htmlRenderer.RenderComponentAsync<TComponent>(parameters);
+            return output.ToHtmlString();
+        });
+
+        return await ToPDFAsync(html, pdfOptions);
+    }
 }
--- DotWebToPDFGeneratorExtension.cs
+++ DotWebToPDFGeneratorExtension.cs
@@ -1,9 +1,4 @@
-﻿using CommunityToolkit.Diagnostics;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Playwright;
-using System.Diagnostics.CodeAnalysis;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace DotWebToPDF;
 
@@ -14,31 +9,5 @@ public static class DotWebToPDFGeneratorExtension
         services.AddScoped<DotWebToPDFGenerator>();
 
         return services;
-    }
-
-    public static async Task<byte[]> ToPDFAsync(this DotWebToPDFGenerator self, string html, PagePdfOptions? pdfOptions = null)
-    {
-        using var playwright = await Playwright.CreateAsync();
-        await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = true });
-        var page = await browser.NewPageAsync();
-        await page.SetContentAsync(html);
-
-        return await page.PdfAsync(pdfOptions);
-    }
-
-    public static async Task<byte[]> ToPDFAsync<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] TComponent>
-        (this DotWebToPDFGenerator self, ParameterView parameters, PagePdfOptions? pdfOptions = null) where TComponent : IComponent
-    {
-        Guard.IsNotNull(parameters);
-
-        await using var htmlRenderer = new HtmlRenderer(self.services, self.loggerFactory);
-
-        var html = await htmlRenderer.Dispatcher.InvokeAsync(async () =>
-        {
-            var output = await htmlRenderer.RenderComponentAsync<TComponent>(parameters);
-            return output.ToHtmlString();
-        });
-
-        return await self.ToPDFAsync(html, pdfOptions);
     }
 }
